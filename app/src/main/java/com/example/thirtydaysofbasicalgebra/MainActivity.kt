103af80b9836ef513789e98bef09ac1ec1caf06d
josikie <kiejosi12@gmail.com>
2024-06-23 17:06:44 +0700

    Added Descriptions

--- app/src/main/java/com/example/thirtydaysofbasicalgebra/MainActivity.kt
+++ app/src/main/java/com/example/thirtydaysofbasicalgebra/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
@@ -22,12 +23,19 @@ import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -35,13 +43,17 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.thirtydaysofbasicalgebra.data.BasicAlgebra
+import com.example.thirtydaysofbasicalgebra.data.BasicAlgebraRepo
 import com.example.thirtydaysofbasicalgebra.ui.theme.ThirtyDaysOfBasicAlgebraTheme
 
 class MainActivity : ComponentActivity() {
@@ -54,19 +66,53 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    BasicAlgebraCardItem()
+                    BasicAlgebraList()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun BasicAlgebraCardItem(modifier: Modifier = Modifier){
+fun ThirtyDaysTopAppBar(modifier: Modifier){
+    CenterAlignedTopAppBar(
+        modifier = modifier.height(70.dp),
+        title = {
+            Image(
+                modifier = Modifier.padding(top = 10.dp),
+                painter = painterResource(id = R.drawable.top_app_logo),
+                contentDescription = stringResource(id = R.string.app_name),
+            )
+        }
+    )
+}
+@Composable
+fun BasicAlgebraList(modifier: Modifier = Modifier,
+                 listLesson: List<BasicAlgebra> = BasicAlgebraRepo.basicAlgebra
+){
+    Scaffold (
+        topBar = {
+            ThirtyDaysTopAppBar(modifier)
+        }
+    ) {
+        LazyColumn(contentPadding = it){
+            items(listLesson.size){i ->
+                BasicAlgebraCardItem(lessonAlgebra = listLesson[i])
+            }
+        }
+    }
+}
+@Composable
+fun BasicAlgebraCardItem(
+    modifier: Modifier = Modifier,
+    lessonAlgebra: BasicAlgebra
+    ){
+    var expanded by remember{ mutableStateOf(false) }
     Card (
         modifier = modifier
             .fillMaxWidth()
-            .padding(24.dp)
+            .padding(start = 24.dp, end = 24.dp, bottom = 24.dp)
         ,
         shape = RoundedCornerShape(16.dp),
         elevation = CardDefaults.cardElevation(defaultElevation = 5.dp),
@@ -80,28 +126,34 @@ fun BasicAlgebraCardItem(modifier: Modifier = Modifier){
             // text day
             Text(
                 modifier = Modifier.padding(start=4.dp, bottom = 4.dp),
-                text = stringResource(id = R.string.title_day_1),
+                text = stringResource(id = lessonAlgebra.title),
                 fontWeight = FontWeight.SemiBold,
                 fontFamily = FontFamily.SansSerif
             )
             // text subtitle
             Text(
                 modifier = Modifier.padding(start=4.dp, top = 8.dp, bottom = 12.dp),
-                text = stringResource(id = R.string.subtitle_1),
+                text = stringResource(id = lessonAlgebra.subTitle),
                 fontWeight = FontWeight.Light,
                 fontFamily = FontFamily.Serif,
                 fontSize = 18.sp
             )
             // image
             Image(
-                painter = painterResource(id = R.drawable.one),
-                contentDescription = stringResource(id = R.string.subtitle_1),
+                painter = painterResource(id = lessonAlgebra.image),
+                contentDescription = stringResource(id = lessonAlgebra.subTitle),
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
                     .clip(RoundedCornerShape(16.dp))
                     .align(Alignment.CenterHorizontally)
                     .size(300.dp)
             )
+        }
+        
+        // description
+        if(expanded){
+            LessonDescription(lessonDescription = lessonAlgebra.description,
+                modifier = Modifier.padding(start = 28.dp, end = 16.dp, top = 16.dp, bottom = 16.dp))
         }
         
         // lines
@@ -113,33 +165,37 @@ fun BasicAlgebraCardItem(modifier: Modifier = Modifier){
         )
 
         // button arrow
-        IconButton(
-            modifier = Modifier.align(Alignment.CenterHorizontally).fillMaxWidth(),
-            onClick = { /*TODO*/ }) {
-            Icon(
-                imageVector = Icons.Filled.KeyboardArrowDown,
-                contentDescription = stringResource(id = R.string.app_name)
-            )
-        }
+        ButtonWider(expanded = expanded, onClick = { expanded = !expanded })
     }
 }
 
 @Composable
-fun CustomLine() {
-    Canvas(modifier = Modifier.fillMaxSize()) {
-        val startX = 20f
-        val startY = 24f
-        val endX = size.width - 20f
-        val endY = size.height
-        drawLine(
-            start = Offset(startX, startY),
-            end = Offset(endX, endY),
-            color = Color.Blue,
-            strokeWidth = 1.dp.toPx(),
-            cap = StrokeCap.Round
+fun ButtonWider(
+    modifier: Modifier = Modifier,
+    expanded: Boolean,
+    onClick:  () -> Unit){
+    IconButton(
+        modifier = modifier.fillMaxWidth(),
+        onClick = onClick
+    ) {
+        Icon(
+            imageVector = if(expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+            contentDescription = stringResource(id = R.string.app_name)
         )
     }
 }
+
+@Composable
+fun LessonDescription(modifier: Modifier = Modifier, lessonDescription: Int){
+    Text(
+        modifier = modifier,
+        text = stringResource(id = lessonDescription),
+        fontWeight = FontWeight.Light,
+        fontFamily = FontFamily.Serif,
+        fontSize = 18.sp
+    )
+}
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -152,6 +208,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ThirtyDaysOfBasicAlgebraTheme {
-        BasicAlgebraCardItem()
+        BasicAlgebraList()
     }
 }
